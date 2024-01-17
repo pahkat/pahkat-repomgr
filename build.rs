ca0f6a56cb563c90342adbbb7e07412859a4c12f
Dylan Hand <dylan.hand@gmail.com>
2024-01-17 14:25:26 +1000

    Build flatbuffers from pahkat-types export
    This, in conjunction with the fix in the pahkat-types `export-flatbuffers` branch allows building pahkat-repomgr without the need for pahkat-types to be checked out in the same parent directory.

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
 use anyhow::Result;
+use std::{env, path::PathBuf, fs::File};
 
 fn main() -> Result<()> {
-    fbs_build::compile_fbs("../pahkat-types/src/index.fbs")
+    let output_path = PathBuf::from(env::var("OUT_DIR")?).join("index.rs");
+    let ugly = false;
+    fbs_build::compile_fbs_generic(
+        ugly,
+        None,
+        Box::new(&pahkat_types::FLATBUFFERS_INDEX[..]),
+        Box::new(File::create(output_path)?),
+    )?;
+    Ok(())
 }
